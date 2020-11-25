9ad7b8093ac1ebb077d1766d793d25ffd9cb6dba
lukrita <rluk3@gatech.com>
2020-11-24 21:49:49 -0800

    Still does not render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import petsInfo from "./petsInfo";
+import { Component } from "react";
+import { render } from "react-dom";
+import Button from "@material-ui/core/Button";
 
 //NOT SURE HOW TO RENDER THE STORE
 const posts = [
@@ -26,22 +29,28 @@ const posts = [
 ];
 
 const store = () => {
-  const allPets = posts.map((pet, idx) => (
+  const allPets = posts.map((info, idx) => (
     <petsInfo
       key={idx}
-      name={pet.name}
-      breed={pet.breed}
-      status={pet.status}
-      gender={pet.gender}
-      yearsOld={pet.yearsOld}
-      adopted={pet.adopted}
+      name={info.name}
+      breed={info.breed}
+      status={info.status}
+      gender={info.gender}
+      yearsOld={info.yearsOld}
+      adopted={info.adopted}
     />
   ));
   return (
-    {allPets}
+    <main>
+        {allPets}
+    </main>
   );
 };
 
+render(<store />, document.getElementById("root"));
+
+
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -49,9 +58,10 @@ ReactDOM.render(
   
   document.getElementById('root')
 );
-
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
+
