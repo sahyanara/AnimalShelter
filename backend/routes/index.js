6c48e91aabd77d8912dc331fa36cc1ad0efc2cc9
ssaravanan9 <ssaravanan9@gatech.edu>
2020-11-28 04:11:34 -0500

    database update (no api call)

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,31 @@
+var { response } = require('express');
 var express = require('express');
 var router = express.Router();
+var app = express();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+async function group(client) {
+  var petsCollection = client.db('Animal_Shelter').collection('Pets');
+  return await petsCollection.aggregate(
+    [
+      {
+        $group:
+        {
+          _id: { $species: '$species' },
+          pets: {
+            $push: {
+              id: '$id', name: '$name', breed: '$breed', status:
+                '$status', gender: '$gender', yearsOld: '$yearsOld', adopted: '$adopted'
+            }
+          }
+        }
+      }
+    ]
+  )
+}
 
 module.exports = router;
